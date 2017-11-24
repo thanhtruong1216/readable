4589f675cc06ea0af7c731e1e44aff1bdcbdefd5
Thanh Thanh <thanhtruong1216@gmail.com>
2017-11-24 18:29:21 +0700

    Create form for add new comment.

--- Frontend/src/Components/Comments.js
+++ Frontend/src/Components/Comments.js
@@ -1,46 +1,69 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { addComment } from '../actions';
 
 class Comments extends Component {
 	state = {
 		comments: null
 	}
 	componentDidMount() {
-		fetch('http://localhost:3001/posts/8xf0y6ziyjabvozdd253nd/comments', { headers: { 'Authorization': 'whatever-you-want'}})
-		.then(results => results.json())
-		.then(({comments}) => {
-			this.setState({comments})
+		const { store }  = this.props;
+		store.subscribe(() => {
+			this.setState({comments: store.getState().comments})
 		})
+	}
+	submitComment = () => {
+		this.props.store.dispatch(addComment({
+			title: this.title.value,
+			author: this.author.value,
+			body: this.body.value
+		}))
+		this.title.value = ''
+		this.author.value = ''
+		this.body.value = ''
 	}
 	render() {
 		const { comments } = this.state;
-		const commentElements = comments && comments.map((comment) => {
-			return(
-				<div key={comment.id}>
-					<div>{comment.author}</div>
-					<div>{comment.body}</div>
-				</div>
-			)
-		})
 		return(
 			<div>
-				<div>{commentElements}</div>
-				<form>
-					<input type="text" placeholder="Create a comment"/>
-					<button>Submit Comment</button>
+				<form onSubmit={(e) => e.preventDefault()}>
+					<input 
+						type="text"
+						placeholder="Title"
+						ref={(input) => this.title = input}
+					/>
+					<input 
+						type="text" 
+						placeholder="Author"
+						ref={(input) => this.author = input}/>
+					<textarea
+						type="text" 
+						placeholder="Comment content"
+						ref={(textarea) => this.body = textarea}>
+					</textarea>
+					<button onClick={this.submitComment}>Submit</button>
 				</form>
+				{comments && comments.map((comment, index) => {
+					return(
+						<div key={index}>
+							<div>{comment.author}</div>
+							<div>{comment.body}</div>
+						</div>
+					)
+				})}
 			</div>
 		);
 	}
 }
 Comments.propTypes = {
-	id: PropTypes.string.isRequired,
-	parentId: PropTypes.string.isRequired,
-	timestamp: PropTypes.number.isRequired,
-	body: PropTypes.string.isRequired,
-	author: PropTypes.string.isRequired,
-	voteScore: PropTypes.number.isRequired,
-	deleted: PropTypes.string.isRequired,
-	parentDeleted: PropTypes.string.isRequired
+		store: PropTypes.object.isRequired,
+// 	id: PropTypes.string.isRequired,
+// 	parentId: PropTypes.string.isRequired,
+// 	timestamp: PropTypes.number.isRequired,
+// 	body: PropTypes.string.isRequired,
+// 	author: PropTypes.string.isRequired,
+// 	voteScore: PropTypes.number.isRequired,
+// 	deleted: PropTypes.string.isRequired,
+// 	parentDeleted: PropTypes.string.isRequired
 }
 export default Comments
