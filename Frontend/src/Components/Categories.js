fd4026121c16829a171d7120c51f532939553f04
Thanh Thanh <thanhtruong1216@gmail.com>
2017-11-24 18:28:47 +0700

    Create form for add new post.

--- Frontend/src/Components/Categories.js
+++ Frontend/src/Components/Categories.js
@@ -2,37 +2,38 @@ import React, { Component } from 'react';
 import Proptypes from 'prop-types';
 
 class Categories extends Component {
-	state = {
-		categories: null
-	}
-	componentDidMount() {
-		fetch('http://localhost:3001/categories', { headers: { 'Authorization': 'whatever-you-want' }})
-		.then(results => results.json())
-		.then(({categories}) => {
-			this.setState({categories})
-			console.log(categories)
-		})
-	}
+	// state = {
+	// 	categories: null
+	// }
+	// componentDidMount() {
+	// 	fetch('http://localhost:3001/categories', { headers: { 'Authorization': 'whatever-you-want' }})
+	// 	.then(results => results.json())
+	// 	.then(({categories}) => {
+	// 		this.setState({categories})
+	// 		console.log(categories)
+	// 	})
+	// }
 	render() {
-		const { categories } = this.state;
-		const categoriesElements = categories && categories.map((category) => {
-			return(
-				<div key={category.path}>
-					<div>{category.name}</div>
-					<div>{category.path}</div>
-				</div>
-			)
-		})
+		// const { categories } = this.state;
+		// const categoriesElements = categories && categories.map((category) => {
+		// 	return(
+		// 		<div key={category.path}>
+		// 			<div>{category.name}</div>
+		// 			<div>{category.path}</div>
+		// 		</div>
+		// 	)
+		// })
 		return(
 			<div>
-				<div>{categoriesElements}</div>
+				{/*<div>{categoriesElements}</div>*/}
+				<div></div>
 			</div>
 		);
 	}
 }
 
-Categories.proptypes = {
-	name: Proptypes.array.isRequire,
-	path: Proptypes.string.isRequire
-}
+// Categories.proptypes = {
+// 	name: Proptypes.array.isRequire,
+// 	path: Proptypes.string.isRequire
+// }
 export default Categories;
--- Frontend/src/Components/Posts.js
+++ Frontend/src/Components/Posts.js
@@ -1,47 +1,85 @@
 import React, { Component } from 'react';
-import Proptypes from 'prop-types';
+import PropTypes from 'prop-types';
+import { addPost } from '../actions'
+import Comments from './Comments';
+import './Posts.css';
 
 class Posts extends Component {
 	state = {
-		posts: null
+		posts: null,
+		showCreatePost: false
 	}
+
 	componentDidMount() {
-		fetch('http://localhost:3001/posts', { headers: { 'Authorization': 'whatever-you-want2'}})
-		.then(results => results.json())
-		.then(({posts}) => {
-			this.setState({posts})
-			console.log(posts)
+		const { store } = this.props
+		store.subscribe(() => {
+			console.log({state: store.getState()})
+			this.setState({posts: store.getState().posts})
 		})
 	}
+
+	submitPost = () => {
+		if(this.title.value === '' || this.author.value === '' || this.body.value === '') {
+			alert('Record not accept empty')
+			return false;
+		}
+		this.props.store.dispatch(addPost({
+			title: this.title.value,
+			author: this.author.value,
+			body: this.body.value,
+		}))
+		this.title.value = ''
+		this.author.value = ''
+		this.body.value = ''
+	}
+
 	render() {
 		const { posts } = this.state;
-		const postsElements = posts && posts.map((post) => {
-			return(
-				<div key={post.id}>
-					<div>{post.author}</div>
-					<div>{post.body}</div>
-				</div>
-			)
-		})
+		const { store } = this.props;
 		return(
-			<div>
-				<div>{postsElements}</div>
-				<form>
-					<input type="text" placeholder="Create a post" />
-					<button>Submit post</button>
+			<div className="main">
+				<form onSubmit={ e => { e.preventDefault() }}>
+					<input 
+						type="text" 
+						placeholder="Title" 
+						ref={(input) => this.title = input}/>
+					<input 
+						type="text"
+						placeholder="Author"
+						ref={(input) => this.author = input}/>
+					<textarea
+						type="text"
+						placeholder="Post content"
+						ref={(textarea) => this.body = textarea}>
+					</textarea>
+					<button onClick={this.submitPost}>Submit</button>
 				</form>
+				{posts && posts.map((post, index) => (
+					<div className="posts-container" key={index}>
+						<div className="title-author-container">
+							<div className='title'>{post.title}</div>
+							<div className='author'>{post.author}</div>
+						</div>
+						<div>{post.body}</div>
+					</div>
+				))}
+				<div>
+					<h3>Create a comment</h3>
+					<Comments store={store}/>
+				</div>
 			</div>
 		);
 	}
 }
 Posts.propTypes = {
-	id: Proptypes.string.isRequired,
-	timestamp: Proptypes.number.isRequired,
-	title: Proptypes.string.isRequired,
-	body: Proptypes.string.isRequired,
-	author: Proptypes.string.isRequired,
-	category: Proptypes.string.isRequired,
-	voteScore: Proptypes.number.isRequired,
-	deleted: Proptypes.string.isRequired
+	store: PropTypes.object.isRequired,
+	// id: PropTypes.string.isRequired,
+	// timestamp: PropTypes.number.isRequired,
+	// title: PropTypes.string.isRequired,
+	// body: PropTypes.string.isRequired,
+	// author: PropTypes.string.isRequired,
+	// category: PropTypes.string.isRequired,
+	// voteScore: PropTypes.number.isRequired,
+	// deleted: PropTypes.string.isRequired
 }
 export default Posts;
